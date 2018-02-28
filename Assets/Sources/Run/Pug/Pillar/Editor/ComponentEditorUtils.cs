4c8b8ad7c97a87880f7b179828927ac47de03d9e
David Samuelson <dxslly@popcornman.attlocal.net>
2018-02-27 23:15:16 -0800

    Added simple editor to offer handles to adjust the entrance and exit of a segment.

---
+++ Assets/Sources/Run/Pug/Pillar/Editor/ComponentEditorUtils.cs
@@ -0,0 +1,26 @@
+﻿using EcsRx.Components;
+using EcsRx.Unity.MonoBehaviours;
+using UnityEditor;
+using UnityEngine;
+
+namespace Sources.Run.Pug.Pillar.Editor {
+  public static class ComponentEditorUtils {
+    public static C GetComponentFromTarget<C>(object target) where C : class, IComponent {
+      if (!(target is GameObject)) {
+        return null;
+      }
+
+      RegisterAsEntity registerAsEntity = (target as GameObject).GetComponent<RegisterAsEntity>();
+      if (null == registerAsEntity) {
+        return null;
+      }
+
+      SerializableEditorEntity editorEntity = registerAsEntity.EditorEntity;
+      if (!editorEntity.HasComponent<C>()) {
+        return null;
+      }
+
+      return editorEntity.GetComponent<C>();
+    }
+  }
+}
---
+++ Assets/Sources/Run/Pug/Pillar/Editor/SegmentHandlers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EcsRx.Components;
+using EcsRx.Unity.MonoBehaviours;
+using Run.Pug.Pillar;
+using Sources.Run.Pug.Pillar.Editor;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameObject))]
+public class SegmentHandlers : Editor {
+
+  private RegisterAsEntity _registerAsEntity;
+
+  private void OnSceneGUI() {
+    Segment segment= ComponentEditorUtils.GetComponentFromTarget<Segment>(target);
+    if (null == segment) {
+      return;
+    }
+    
+    EditorGUI.BeginChangeCheck();
+    Vector3 entrancePosition = Handles.PositionHandle(segment.Entrance, Quaternion.identity);
+    Handles.Label(entrancePosition, "Entrance");
+    if (EditorGUI.EndChangeCheck()) {
+      segment.Entrance = entrancePosition;
+    }
+    
+    EditorGUI.BeginChangeCheck();
+    Vector3 exitPosition = Handles.PositionHandle(segment.Exit, Quaternion.identity);
+    Handles.Label(exitPosition, "Exit");
+    if (EditorGUI.EndChangeCheck()) {
+      segment.Exit = exitPosition;
+    }
+  }
+}
